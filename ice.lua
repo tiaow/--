7f1d9f6cf7db4c58933037c3197bb09a2c3a4036
tiaow <3696812003@qq.com>
2026-07-13 18:44:40 +0800

    创建 ice.lua

---
+++ ice.lua
@@ -0,0 +1,15 @@
+credits:Toggle("自动互动", "Auto Interact", false, function(state)
+        if state then
+            autoInteract = true
+            while autoInteract do
+                for _, descendant in pairs(workspace:GetDescendants()) do
+                    if descendant:IsA("ProximityPrompt") then
+                        fireproximityprompt(descendant)
+                    end
+                end
+                task.wait(0.25) 
+            end
+        else
+            autoInteract = false
+        end
+    end)
