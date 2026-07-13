11de9d3dc297162f811ed5d8c9cb128c596158c6
tiaow <3696812003@qq.com>
2026-07-13 19:01:02 +0800

    更新 ice.lua

--- ice.lua
+++ ice.lua
@@ -5,8 +5,33 @@ local Player = window:Tab("玩家",'')
 local Main = window:Tab("主要",'')
 local Cheng = window:Tab("成就",'')
 
-local Xing = creds:section("作者信息",true)
-Xing:Lable
+
+local Xing1 = creds:section("作者信息",true)
+
+local Xing = creds:section("设置",true)
+        Xing:Toggle("移除UI辉光", "", false, function(state)
+            if state then
+                game:GetService("CoreGui")["frosty is cute"].Main.DropShadowHolder.Visible = false
+            else
+                game:GetService("CoreGui")["frosty is cute"].Main.DropShadowHolder.Visible = true
+            end
+    end)
+
+
+        Xing:Toggle("彩虹UI", "", false, function(state)
+            if state then
+                game:GetService("CoreGui")["frosty is cute"].Main.Style = "DropShadow"
+            else
+                game:GetService("CoreGui")["frosty is cute"].Main.Style = "Custom"
+            end
+    end)
+
+    
+        Xing:Button("摧毁GUI",function()
+                game:GetService("CoreGui")["frosty is cute"]:Destroy()
+    end)
+
+
 
 :Toggle("自动互动", "Auto Interact", false, function(state)
         if state then
