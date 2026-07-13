25bcfd8466da420d13638eb265ca07e92ee2ae17
tiaow <3696812003@qq.com>
2026-07-13 18:50:31 +0800

    更新 ice.lua

--- ice.lua
+++ ice.lua
@@ -1,4 +1,12 @@
-credits:Toggle("自动互动", "Auto Interact", false, function(state)
+local library = loadstring(game:HttpGet("https://github.com/tiaow/gb/blob/main/%E5%BD%A9%E8%89%B2ui"))()
+local window = library:new("冰工厂2")
+local Xing = window:Tab("信息",'')
+local Player = window:Tab("玩家",'')
+local Main = window:Tab("主要",'')
+local Cheng = window:Tab("成就",'')
+
+
+:Toggle("自动互动", "Auto Interact", false, function(state)
         if state then
             autoInteract = true
             while autoInteract do
